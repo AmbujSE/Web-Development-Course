7d81e3a7924cc7e6679011c39efdd0cdef0d5db9
AmbujSE <blastedaway94@gmail.com>
2024-02-23 22:03:42 +0530

    Initial Commit

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,24 @@
-$("h1").css("color", "red");
+$('h1').click(function(){
+    $('h1').addClass('big-title');
+    $("h1").css("color", "purple");
+});
+
+$('button').on('click', function(){
+    // $('h1').hide();
+    // $("h1").toggle();
+    // $("h1").fadeOut();
+    // $("h1").fadeToggle();
+    // $("h1").slideUp();
+    // $("h1").slideToggle();
+    $('h1').animate({opacity: 0.5});
+})
+
+// $(document).keypress(function(event) {
+//     // console.log(event.key);
+//     $('h1').text(event.key);
+// })
+
+// $('h1').mouseover(function(){
+//     $('h1').addClass('big-title');
+//     $("h1").css("color", "purple");
+// });
